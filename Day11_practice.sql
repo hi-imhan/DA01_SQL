acd237f2b38c07b36335d684604ec9f38dff2c36
hi-imhan <153355616+hi-imhan@users.noreply.github.com>
2023-12-18 21:08:04 +0700

    Create Day11_practice.sql

---
+++ Day11_practice.sql
@@ -0,0 +1,64 @@
+-- bt 1
+SELECT country.continent, 
+FLOOR(AVG(city.population))
+FROM country
+INNER JOIN city 
+ON country.code=city.countrycode
+GROUP BY country.continent
+
+-- bt 2
+SELECT 
+ROUND(COUNT(texts.email_id)::decimal/COUNT(emails.email_id),2) AS active_rate
+FROM emails
+LEFT JOIN texts
+ON emails.email_id = texts.email_id
+AND texts.signup_action = 'Confirmed';
+
+-- bt 3
+SELECT b.age_bucket,
+ROUND(SUM(CASE WHEN activity_type='send'THEN time_spent END)/(SUM(CASE WHEN activity_type='open'THEN time_spent END)+ SUM(CASE WHEN activity_type='send'THEN time_spent END))*100,2) as send_perc,
+ROUND(SUM(CASE WHEN activity_type='open'THEN time_spent END)/(SUM(CASE WHEN activity_type='open'THEN time_spent END)+SUM(CASE WHEN activity_type='send'THEN time_spent END))*100,2) as open_perc
+FROM activities AS a  
+INNER JOIN age_breakdown AS b  
+ON a.user_id=b.user_id
+WHERE activity_type IN('send','open')
+GROUP BY b.age_bucket
+
+-- bt 4
+SELECT a.customer_id
+FROM customer_contracts AS a  
+LEFT JOIN products AS b  
+ON a.product_id=b.product_id
+GROUP BY a.customer_id
+HAVING COUNT(DISTINCT b.product_category)=3
+
+-- bt 5
+# Write your MySQL query statement below
+SELECT emp.employee_id, emp.name, 
+COUNT(rep.name) AS reports_count, ROUND(AVG(rep.age),0) AS average_age
+FROM employees AS emp
+LEFT JOIN employees AS rep
+ON emp.employee_id=rep.reports_to
+GROUP BY emp.employee_id, emp.name
+HAVING COUNT(rep.name)>=1
+ORDER BY emp.employee_id
+
+-- bt 6
+/*output: name of products >= 100 units orders Feb 2020, amount*/
+SELECT products.product_name,
+SUM(orders.unit) AS unit
+FROM products 
+INNER JOIN orders
+ON products.product_id=orders.product_id
+WHERE EXTRACT(MONTH FROM orders.order_date)=2 AND orders.order_date<'2020-03-01'
+GROUP BY products.product_name
+HAVING SUM(orders.unit)>=100
+
+-- bt 7
+/*output: Page ID with 0 like, sort by page ID*/
+SELECT pages.page_id
+FROM pages
+LEFT JOIN page_likes
+ON pages.page_id=page_likes.page_id
+WHERE page_likes.user_id IS NULL
+ORDER BY pages.page_id
